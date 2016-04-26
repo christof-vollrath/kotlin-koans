5788de7ae76cd823c0f70a4db84e022837051f4c
Christof Vollrath <christof.vollrath@bild.de>
2016-04-26 15:00:07 +0200

    _25_Comparison.kt completed

--- src/iii_conventions/_25_Comparison_.kt
+++ src/iii_conventions/_25_Comparison_.kt
@@ -15,7 +15,12 @@ fun todoTask25(): Nothing = TODO(
 )
 
 fun task25(date1: MyDate, date2: MyDate): Boolean {
-    todoTask25()
-//    return date1 < date2
+    return date1 < date2
 }
+
+operator fun MyDate.compareTo(other: MyDate): Int =
+        if (year != other.year) year - other.year
+        else if (month != other.month) month - other.month
+        else dayOfMonth - other.dayOfMonth
+
 
