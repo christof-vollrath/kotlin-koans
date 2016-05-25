a66a7e68f195971f77c285c424c8975752d91f38
Christof Vollrath <christof.vollrath@bild.de>
2016-05-25 18:46:05 +0200

    _31_Invoke.kt completed

--- test/iii_conventions/_31_Invoke.kt
+++ test/iii_conventions/_31_Invoke.kt
@@ -20,6 +20,7 @@ class _31_Invoke {
         testInvokable(0) { it }
     }
 
-    operator fun Invokable.invoke() = todoTask31()
-    fun Invokable.getNumberOfInvocations() = todoTask31()
 }
+
+operator fun Invokable.invoke(): Invokable { numberOfInvocations++; return this }
+fun Invokable.getNumberOfInvocations() = numberOfInvocations
